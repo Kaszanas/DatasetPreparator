2969e0787ad31a8e2458bd451db3fe40f74400ed
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-01-06 21:56:36 +0100

    refactor(user_prompt.py): added logging

--- src/datasetpreparator/utils/user_prompt.py
+++ src/datasetpreparator/utils/user_prompt.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import logging
 
 
-def user_prompt_overwrite_ok(filepath: Path, force: bool) -> bool:
+def user_prompt_overwrite_ok(path: Path, force: bool) -> bool:
     """
     Prompts the user to confirm if they want to potentially overwrite a file or directory if
     it already exists.
@@ -26,11 +27,13 @@ def user_prompt_overwrite_ok(filepath: Path, force: bool) -> bool:
 
     # File or directory does not exist, so it can be created,
     # there is no risk of overwriting anything:
-    if not filepath.exists():
+    if not path.exists():
+        logging.debug(f"Path {str(path.resolve())} does not exist. Safe to create.")
         return True
 
     # Directory is empty, so it can be overwritten without any risk:
-    if filepath.is_dir() and len(list(filepath.iterdir())) == 0:
+    if path.is_dir() and len(list(path.iterdir())) == 0:
+        logging.debug(f"Directory {str(path.resolve())} is empty. Safe to overwrite.")
         return True
 
     return_map = {
@@ -40,13 +43,17 @@ def user_prompt_overwrite_ok(filepath: Path, force: bool) -> bool:
 
     # File or directory exists, so we need to prompt the user to confirm
     # if they want to potentially overwrite something and loose data:
-    print(f"File {filepath} already exists.")
+    print(f"File {path} already exists.")
     user_input = input("Do you want to overwrite it? (y/n): ")
 
     if user_input.lower() in return_map:
+        logging.debug(
+            f"User input: {user_input.lower()}, returning: {return_map[user_input.lower()]}"
+        )
         return return_map[user_input.lower()]
 
+    logging.debug(
+        f"Invalid input provided: {user_input.lower()}, calling the function recursively."
+    )
     print("Invalid input, please type 'y' or 'n'.")
-    return user_prompt_overwrite_ok(filepath, force)
-
-    return False
+    return user_prompt_overwrite_ok(path, force)
