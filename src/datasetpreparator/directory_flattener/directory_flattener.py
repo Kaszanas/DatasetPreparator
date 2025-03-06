1a83a76e03e62c8d7deeaa4da6a84785b6225c62
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-03-06 22:19:08 +0100

    feat(directory_flattener): multithreading file copying

--- src/datasetpreparator/directory_flattener/directory_flattener.py
+++ src/datasetpreparator/directory_flattener/directory_flattener.py
@@ -1,16 +1,29 @@
+from concurrent.futures import ThreadPoolExecutor
+import hashlib
+import json
+import logging
+import shutil
+from multiprocessing import freeze_support
 from pathlib import Path
 from typing import Dict, List, Tuple
-import json
-import shutil
-import logging
-import hashlib
-
 
 import click
 from tqdm import tqdm
 
 from datasetpreparator.settings import LOGGING_FORMAT
 from datasetpreparator.utils.user_prompt import user_prompt_overwrite_ok
+
+
+class MultiprocessFlattenArguments:
+    def __init__(
+        self,
+        dir_output_path: Path,
+        maybe_dir: Path,
+        files_with_extension: List[Path],
+    ):
+        self.dir_output_path = dir_output_path
+        self.maybe_dir = maybe_dir
+        self.files_with_extension = files_with_extension
 
 
 def save_dir_mapping(output_path: Path, dir_mapping: dict) -> None:
@@ -82,7 +95,7 @@ def directory_flatten(
     dir_structure_mapping = {}
     for file in tqdm(
         list_of_files,
-        desc=f"Flattening directory {root_directory.name}",
+        desc=f"Flattening {root_directory.name:<35}",
         unit="file",
     ):
         # Getting the ReplayPack/directory/structure/file.SC2Replay path,
@@ -116,8 +129,73 @@ def directory_flatten(
     return dir_structure_mapping
 
 
+def multiprocess_directory_flattener(
+    directories_to_process: List[MultiprocessFlattenArguments],
+    n_processes: int,
+) -> List[Path]:
+    """
+    Multiprocesses the directory flattening.
+
+    Parameters
+    ----------
+    directories_to_process : List[MultiprocessFlattenArguments]
+        List of the arguments corresponding to the directories that will be processed.
+    n_processes : int
+        Number of processes that will be spawned.
+
+    Returns
+    -------
+    List[Path]
+        Returns a list of paths to the output directories.
+    """
+
+    with ThreadPoolExecutor(
+        max_workers=n_processes,
+        initializer=tqdm.set_lock,
+        initargs=(tqdm.get_lock(),),
+    ) as executor:
+        results = list(executor.map(flatten_save_dir_mapping, directories_to_process))
+
+    return list(results)
+
+
+def flatten_save_dir_mapping(
+    arguments: MultiprocessFlattenArguments,
+) -> Path:
+    """
+    Flattens the directory and saves the mapping.
+
+    Parameters
+    ----------
+    arguments : MultiprocessFlattenArguments
+        Specifies the arguments as per the MultiprocessFlattenArguments class fields.
+
+    Returns
+    -------
+    Path
+        Returns the path to the output directory.
+    """
+
+    dir_structure_mapping = directory_flatten(
+        root_directory=arguments.maybe_dir,
+        list_of_files=arguments.files_with_extension,
+        dir_output_path=arguments.dir_output_path,
+    )
+
+    save_dir_mapping(
+        output_path=arguments.dir_output_path,
+        dir_mapping=dir_structure_mapping,
+    )
+
+    return arguments.dir_output_path
+
+
 def multiple_directory_flattener(
-    input_path: Path, output_path: Path, file_extension: str, force_overwrite: bool
+    input_path: Path,
+    output_path: Path,
+    file_extension: str,
+    n_processes: int,
+    force_overwrite: bool,
 ) -> Tuple[bool, List[Path]]:
     """
     Provides the main logic for "directory flattening".
@@ -139,6 +217,8 @@ def multiple_directory_flattener(
     file_extension : str
         Specifies extension for which the detected files will be brought \
         up to the top level of the "flattened" directory
+    n_processes : int
+        Specifies the number of processes that will be spawned.
     force : bool
         Specifies if the user wants to overwrite the output directory without \
         being prompted.
@@ -165,8 +245,16 @@ def multiple_directory_flattener(
         output_path.mkdir(exist_ok=True)
 
     output_directories = []
+    directories_to_process = []
+
     # Iterate over directories:
-    for item in input_path.iterdir():
+
+    directory_contants = list(input_path.iterdir())
+    for item in tqdm(
+        directory_contants,
+        desc="Creating output directories",
+        unit="dir",
+    ):
         maybe_dir = Path(input_path, item).resolve()
         if not maybe_dir.is_dir():
             logging.debug(f"Skipping {str(maybe_dir)}, not a directory.")
@@ -185,19 +273,18 @@ def multiple_directory_flattener(
         ):
             logging.debug(f"Creating directory {str(dir_output_path)}, didn't exist.")
             dir_output_path.mkdir(exist_ok=True)
-
-            dir_structure_mapping = directory_flatten(
-                root_directory=maybe_dir,
-                list_of_files=files_with_extension,
-                dir_output_path=dir_output_path,
+            directories_to_process.append(
+                MultiprocessFlattenArguments(
+                    dir_output_path=dir_output_path,
+                    maybe_dir=maybe_dir,
+                    files_with_extension=files_with_extension,
+                )
             )
 
-            save_dir_mapping(
-                output_path=dir_output_path,
-                dir_mapping=dir_structure_mapping,
-            )
-
-        output_directories.append(dir_output_path)
+    multiprocess_directory_flattener(
+        directories_to_process=directories_to_process,
+        n_processes=n_processes,
+    )
 
     return (True, output_directories)
 
@@ -237,6 +324,13 @@ def multiple_directory_flattener(
     help="File extension for the files that will be put to the top level directory. Example ('.SC2Replay').",
 )
 @click.option(
+    "--n_processes",
+    type=int,
+    default=1,
+    required=False,
+    help="Number of processes to use for multiprocessing.",
+)
+@click.option(
     "--force_overwrite",
     type=bool,
     default=False,
@@ -253,6 +347,7 @@ def main(
     input_path: Path,
     output_path: Path,
     file_extension: str,
+    n_processes: int,
     log: str,
     force_overwrite: bool,
 ) -> None:
@@ -265,9 +360,11 @@ def main(
         input_path=input_path,
         output_path=output_path,
         file_extension=file_extension,
+        n_processes=n_processes,
         force_overwrite=force_overwrite,
     )
 
 
 if __name__ == "__main__":
+    freeze_support()  # For Windows support of parallel tqdm
     main()
